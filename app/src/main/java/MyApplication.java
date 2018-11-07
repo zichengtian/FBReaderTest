3f65fab48c7293e0e0286be3811062d93be58590
zichengtian <15829230025@163.com>
2018-11-07 18:09:23 +0800

    Initial commit

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,9 @@
+import org.geometerplus.android.fbreader.FBReaderApplication;
+
+/**
+ * 作者：信仰 on 2018/11/7 16:43
+ * 邮箱：15829230025@163.com
+ */
+public class MyApplication extends FBReaderApplication {
+
+}
